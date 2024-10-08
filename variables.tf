c90db198aa027cf65828477f46da5bd0e4980665
Amit Pratap Singh <87903560+a2singh@users.noreply.github.com>
2024-10-09 00:21:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,49 +1,43 @@
-## REQUIRED VARIABLES
-
 variable "region" {
-
+  description = "AWS Region"
 }
-variable "access_key"{
 
+variable "access_key" {
+  description = "AWS Access Key"
 }
+
 variable "secret_key" {
-
+  description = "AWS Secret Key"
 }
 
 variable "default_cache_behavior" {
   description = "The default cache behavior for this distribution"
   type        = any
   default     = {
-  target_origin_id       = "example-origin-1"
-  viewer_protocol_policy = "redirect-to-https"
-}
-
+    target_origin_id       = "example-origin-1"
+    viewer_protocol_policy = "redirect-to-https"
+  }
 }
 
 variable "geo_restriction" {
   description = "The restriction configuration for this distribution (geo_restrictions)"
   type        = any
   default     = {
-  restriction_type = "whitelist"
-  locations = ["US", "CA"]
-}
-
+    restriction_type = "whitelist"
+    locations = ["US", "CA"]
+  }
 }
 
 variable "origin" {
   description = "One or more origins for this distribution (multiples allowed)."
   type        = any
   default     = {
-  "example-origin-1" = {
-    domain_name  = "example-bucket.s3.amazonaws.com"
-    # s3_origin_config = {
-    #   cloudfront_access_identity_path = "origin-access-identity/cloudfront/E1234567890"
-    # }
-  }
+    "example-origin-1" = {
+      domain_name  = "example-bucket.s3.amazonaws.com"
+    }
   }
 }
 
-## OPTIONAL VARIABLES
 variable "aliases" {
   description = "Extra CNAMEs (alternate domain names), if any, for this distribution."
   type        = list(string)
@@ -87,7 +81,7 @@ variable "custom_error_response" {
 }
 
 variable "default_root_object" {
-  description = "The object that you want CloudFront to return (for example, index.html) when an end user requests the root URL."
+  description = "The object that you want CloudFront to return when an end user requests the root URL."
   type        = string
   default     = null
 }
@@ -98,9 +92,8 @@ variable "enabled" {
   default     = true
 }
 
-
 variable "http_version" {
-  description = "The maximum HTTP version to support on the distribution. Allowed values are http1.1 and http2. The default is http2."
+  description = "The maximum HTTP version to support on the distribution."
   type        = string
   default     = "http2"
 }
@@ -112,7 +105,7 @@ variable "is_ipv6_enabled" {
 }
 
 variable "logging_config" {
-  description = "The logging configuration that controls how logs are written to your distribution (maximum one)."
+  description = "The logging configuration that controls how logs are written to your distribution."
   type        = any
   default     = {}
 }
@@ -125,7 +118,6 @@ variable "origin_access_control" {
     signing_behavior = string
     signing_protocol = string
   }))
-
   default = {
     s3 = {
       description      = "S3 buck",
@@ -137,40 +129,39 @@ variable "origin_access_control" {
 }
 
 variable "origin_access_identities" {
-  description = "Map of CloudFront origin access identities (value as a comment)"
+  description = "Map of CloudFront origin access identities"
   type        = map(string)
   default     = {
-  "example-identity" = "My CloudFront Origin Access Identity"
-}
-
+    "example-identity" = "My CloudFront Origin Access Identity"
+  }
 }
 
 variable "origin_group" {
-  description = "One or more origin_group for this distribution (multiples allowed)."
+  description = "One or more origin_group for this distribution."
   type        = any
   default     = {}
 }
 
 variable "ordered_cache_behavior" {
-  description = "An ordered list of cache behaviors resource for this distribution. List from top to bottom in order of precedence. The topmost cache behavior will have precedence 0."
+  description = "An ordered list of cache behaviors resource for this distribution."
   type        = any
   default     = []
 }
 
 variable "price_class" {
-  description = "The price class for this distribution. One of PriceClass_All, PriceClass_200, PriceClass_100"
+  description = "The price class for this distribution."
   type        = string
   default     = null
 }
 
 variable "realtime_metrics_subscription_status" {
-  description = "A flag that indicates whether additional CloudWatch metrics are enabled for a given CloudFront distribution. Valid values are `Enabled` and `Disabled`."
+  description = "Indicates whether additional CloudWatch metrics are enabled for a given CloudFront distribution."
   type        = string
   default     = "Enabled"
 }
 
 variable "retain_on_delete" {
-  description = "Disables the distribution instead of deleting it when destroying the resource through Terraform. If this is set, the distribution needs to be deleted manually afterwards."
+  description = "Disables the distribution instead of deleting it when destroying."
   type        = bool
   default     = false
 }
@@ -184,20 +175,25 @@ variable "tags" {
 variable "viewer_certificate" {
   description = "The SSL configuration for this distribution"
   type        = any
-  default = {
+  default     = {
     cloudfront_default_certificate = true
     minimum_protocol_version       = "TLSv1"
   }
 }
 
 variable "wait_for_deployment" {
-  description = "If enabled, the resource will wait for the distribution status to change from InProgress to Deployed. Setting this tofalse will skip the process."
+  description = "If enabled, the resource will wait for the distribution status to change from InProgress to Deployed."
   type        = bool
   default     = true
 }
 
 variable "web_acl_id" {
-  description = "If you're using AWS WAF to filter CloudFront requests, the Id of the AWS WAF web ACL that is associated with the distribution. The WAF Web ACL must exist in the WAF Global (CloudFront) region and the credentials configuring this argument must have waf:GetWebACL permissions assigned. If using WAFv2, provide the ARN of the web ACL."
+  description = "The Id of the AWS WAF web ACL associated with the distribution."
   type        = string
   default     = null
+}
+
+locals {
+  create_origin_access_identity = var.create_origin_access_identity && length(keys(var.origin_access_identities)) > 0
+  create_origin_access_control  = var.create_origin_access_control && length(keys(var.origin_access_control)) > 0
 }
